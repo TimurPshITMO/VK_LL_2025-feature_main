7f200558caf74545fc1fb3128379d41ad3297fb5
TimurP <timurpshinshev@gmail.com>
2025-02-19 00:52:09 +0300

    still fixing

--- server/server.py
+++ server/server.py
@@ -41,7 +41,19 @@ async def lifespan(app: FastAPI):
     yield
     logger.info("Shutting down the server...")
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
